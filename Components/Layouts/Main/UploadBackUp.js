941109ba73164354de271a90a2e821eeaa91e746
Marksman1593 <bilalazfar1593@gmail.com>
2026-03-02 08:35:23 +0500

    AEBL Fix

--- Components/Layouts/Main/UploadBackUp.js
+++ Components/Layouts/Main/UploadBackUp.js
@@ -45,7 +45,8 @@ const Upload_CoA = () => {
             console.log("Getting Air Jobs Data for Fix")
             const result = await axios.get("http://localhost:8081/jobs/fixJobs");
             console.log("Fix Jobs Result:", result.data.result)
-            await axios.post("http://localhost:8084/seaJob/fixAirJobs", result.data.result)
+            // await axios.post("http://localhost:8084/seaJob/fixAirJobs", result.data.result)
+            await axios.post("http://localhost:8084/seaJob/fixAEBL", result.data.result.BL)
         }catch(e){
             console.error(e)
         }

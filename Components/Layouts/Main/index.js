57e08b2831abf9356241133e163406a1ac85d1e8
Marksman1593 <bilalazfar1593@gmail.com>
2026-05-19 21:20:59 +0500

    Update

--- Components/Layouts/Main/index.js
+++ Components/Layouts/Main/index.js
@@ -44,8 +44,8 @@ const Main = ({ sessionData, chartData }) => {
       {/* {username == 'Saad' && <VannaChat /> } */}
       {/* <OllamaChat/> */}
       {/* {username == 'Saad' && <UploadBackUp /> } */}
-      {/* s<UploadBackUp /> */}
-      {audit && <AuditLog /> }
+      <UploadBackUp />
+      {/* {audit && <AuditLog /> } */}
     </div>
   );
 };
